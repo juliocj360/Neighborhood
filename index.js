b382d54bb8ad72a2a12d10fd04363da3dcd62358
juliocj360 <juliocj360@gmail.com>
2017-01-17 10:45:32 -0800

    User can view other makers on the map. #2

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+const mongoTest = require('./app.js');
+const bodyParser = require("body-parser");
 
 const PORT = 3000;
 
@@ -8,3 +10,10 @@ app.listen(PORT, () => {
 });
 
 app.use(express.static('public'));
+app.use(bodyParser.json());
+
+app.get('/markers', (req, res) => {
+  mongoTest.finder(function(docs) {
+    res.json(docs);
+  })
+})
